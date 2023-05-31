5e6831de162122e8e02e1dba61a12d22b4d22522
Euro Chaves <eurocunhachaves@gmail.com>
2023-05-31 15:18:54 -0300

    Organizando Funções

---
+++ Funcao.java
@@ -0,0 +1,72 @@
+package JogoDaVelha;
+
+public class Funcao {
+
+    public static void realizaJogada(String[][] tabela, int posicao, int jogador){
+        switch(posicao){
+            case 1:
+                if(jogador % 2 > 0){
+                    tabela[5][1] = " X ";
+                } else tabela[5][1] = " O ";
+                break;
+
+            case 2:
+                if(jogador % 2 > 0){
+                    tabela[5][3] = " X ";
+                } else tabela[5][3] = " O ";
+                break;
+
+            case 3:
+                if(jogador % 2 > 0){
+                    tabela[5][5] = " X ";
+                } else tabela[5][5] = " O ";
+                break;
+
+            case 4:
+                if(jogador % 2 > 0){
+                    tabela[3][1] = " X ";
+                } else tabela[3][1] = " O ";
+                break;
+
+            case 5:
+                if(jogador % 2 > 0){
+                    tabela[3][3] = " X ";
+                } else tabela[3][3] = " O ";
+                break;
+
+            case 6:
+                if(jogador % 2 > 0){
+                    tabela[3][5] = " X ";
+                } else tabela[3][5] = " O ";
+                break;
+
+            case 7:
+                if(jogador % 2 > 0){
+                    tabela[1][1] = " X ";
+                } else tabela[1][1] = " O ";
+                break;
+
+            case 8:
+                if(jogador % 2 > 0){
+                    tabela[1][3] = " X ";
+                } else tabela[1][3] = " O ";
+                break;
+
+            case 9:
+                if(jogador % 2 > 0){
+                    tabela[1][5] = " X ";
+                } else tabela[1][5] = " O ";
+                break;
+        }
+    }
+
+    public static void imprimeTabela(String[][] tabela){
+        //Imprime a figura
+        for(String[] row: tabela){
+            for(String s : row) {
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+}
+}
--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -12,7 +12,7 @@ public class JogoDaVelha {
                              {"|", "   ", "|", "   ", "|", "   ", "|"},
                              {"|", "---", "|","---", "|","---", "|"}};
 
-        imprimeTabela(tabela);
+        Funcao.imprimeTabela(tabela);
 
         //Recebe a jogada do usuario
         Scanner scanner = new Scanner(System.in);
@@ -21,83 +21,14 @@ public class JogoDaVelha {
         scanner.close();
 
         int jogador = 1;
-        switch(posicao){
-            case 1:
-                if(jogador % 2 > 0){
-                    tabela[5][1] = " X ";
-                }
-                else tabela[5][1] = " O ";
-                break;
 
-            case 2:
-                if(jogador % 2 > 0){
-                    tabela[5][3] = " X ";
-                }
-                else tabela[5][3] = " O ";
-                break;
+        Funcao.realizaJogada(tabela, posicao, jogador);
 
-            case 3:
-                if(jogador % 2 > 0){
-                    tabela[5][5] = " X ";
-                }
-                else tabela[5][5] = " O ";
-                break;
-
-            case 4:
-                if(jogador % 2 > 0){
-                    tabela[3][1] = " X ";
-                }
-                else tabela[3][1] = " O ";
-                break;
-
-            case 5:
-                if(jogador % 2 > 0){
-                    tabela[3][3] = " X ";
-                }
-                else tabela[3][3] = " O ";
-                break;
-
-            case 6:
-                if(jogador % 2 > 0){
-                    tabela[3][5] = " X ";
-                }
-                else tabela[3][5] = " O ";
-                break;
-
-            case 7:
-                if(jogador % 2 > 0){
-                    tabela[1][1] = " X ";
-                }
-                else tabela[1][1] = " O ";
-                break;
-
-            case 8:
-                if(jogador % 2 > 0){
-                    tabela[1][3] = " X ";
-                }
-                else tabela[1][3] = " O ";
-                break;
-
-            case 9:
-                if(jogador % 2 > 0){
-                    tabela[1][5] = " X ";
-                }
-                else tabela[1][5] = " O ";
-                break;
-        }
-
-        imprimeTabela(tabela);
+        Funcao.imprimeTabela(tabela);
     }
 
-    public static void imprimeTabela(String[][] tabela){
-        //Imprime a figura
-        for(String[] row: tabela){
-            for(String s : row) {
-                System.out.print(s);
-            }
-            System.out.println();
-    }
+
 
         
 }
-}
+
