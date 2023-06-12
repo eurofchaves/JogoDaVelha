69f4be149bc8a408425dc76e0783b0b91d9a2fd8
AlecrimLunar <129176336+AlecrimLunar@users.noreply.github.com>
2023-06-12 13:03:09 -0300

    Criação do objeto humano em um arquivo diferente

---
+++ Humano.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Humano extends Jogador {
+    
+    private String nome;
+
+    public Humano() {}
+    public Humano (String nome, List<Integer> jogadas){
+        super(jogadas);
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
