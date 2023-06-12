b0cb5600c75a836f527010187c53f69faba87b94
AlecrimLunar <129176336+AlecrimLunar@users.noreply.github.com>
2023-06-12 13:07:12 -0300

    Saida do humano e bot do mesmo arquivo do arquivo jogador

--- Jogador.java
+++ Jogador.java
@@ -1,39 +1,24 @@
 package JogoDaVelha;
 import java.util.ArrayList;
-import java.util.Random;
 import java.util.List;
 
 public abstract class Jogador {
-    protected List<Integer> jogadas = new ArrayList<>();
-}
 
-class Humano extends Jogador{
-    public String nome;
-    public Humano(){};
-    public Humano(String nome, List<Integer> jogadas){
-        this.nome = nome;
-        this.jogadas = jogadas;
-    }
-}
-
-class Bot extends Jogador{
-    private Random random = new Random();
-    Bot(){};
-    Bot(List<Integer> jogadas){
-        this.jogadas = jogadas;
-    }
-    public Integer geraJogada(List<Integer> jogadasH, List<Integer> jogadasB){
-        boolean numeroRepetido = false;
-        int numeroGerado;
-        do{
-            numeroGerado = random.nextInt(9) + 1;
-            for (int jogada : jogadas) {
-                if (jogada == numeroGerado) {
-                    numeroRepetido = true;
-                }
-            }
-        }while(numeroRepetido);
+    private List<Integer> jogadas = new ArrayList<>();
     
-        return numeroGerado;
+    public Jogador() {}
+    public Jogador(List<Integer> jogadas) {
+        this.jogadas = jogadas;
+    }
+
+    public List<Integer> getJogadas() {
+        return this.jogadas;
+    }
+    public void setJogadas(List<Integer> jogadas) {
+        this.jogadas = jogadas;
+    }
+    
+    public void addJogada(int jogada) {
+        jogadas.add(jogada);
     }
 }
